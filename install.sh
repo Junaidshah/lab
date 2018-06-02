f5d483c621032e831251f4cfd80ee15df63d95b7
Junaidshah <junaid.zealot20@gmail.com>
2018-06-02 23:01:24 +0800

    Improving the installation script to make sure vagrant is up to date.

--- install.sh
+++ install.sh
@@ -4,7 +4,8 @@ set -e
 if ! type "brew" > /dev/null; then
   ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)";
 fi
-brew cask install vagrant;
+brew cask reinstall vagrant
+vagrant plugin expunge --reinstall
 brew cask reinstall --force virtualbox
 
 #Bring up the virtual machine with wordpress and nagios server installed.
